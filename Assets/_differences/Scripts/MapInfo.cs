e858056b39ea440d5fcce6c6ffd5762acde24f8d
Oleg Yanushevsky <asofi94@gmail.com>
2020-05-26 19:20:36 +0300

    Setup map movement

--- Assets/_differences/Scripts/MapInfo.cs
+++ Assets/_differences/Scripts/MapInfo.cs
@@ -27,7 +27,6 @@ public class MapInfo : MonoBehaviour {
         for (int i = 0; i < _levelCount; i++) {
             Gizmos.color = Color.yellow;
             Gizmos.DrawIcon(_pathCreator.path.GetPointAtTime(step * i + step * 0.5f) + Vector3.up * 0.5f, "Misc/CupGizmo.png");
-            // Gizmos.DrawSphere(_pathCreator.path.GetPointAtTime(step * i + step * 0.5f), 0.2f);
         }
     }
 }
