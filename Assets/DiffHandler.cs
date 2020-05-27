6663cd311672fd8c97791adb6d7d5940c48f64ff
Eugene <me@airion.games>
2020-05-27 16:09:57 +0300

    Added json loading

--- Assets/DiffHandler.cs
+++ Assets/DiffHandler.cs
@@ -2,27 +2,8 @@
 
 using UnityEngine;
 
-public class DiffHandler : MonoBehaviour, IEquatable<DiffHandler> {
+public class DiffHandler : MonoBehaviour {
     public Vector2 ImageSpaceCoordinates;
     public int Id;
     public float Radius = 1;
-
-    public bool Equals(DiffHandler other) {
-        if (ReferenceEquals(null, other)) return false;
-        if (ReferenceEquals(this, other)) return true;
-        return base.Equals(other) && Id == other.Id;
-    }
-
-    public override bool Equals(object obj) {
-        if (ReferenceEquals(null, obj)) return false;
-        if (ReferenceEquals(this, obj)) return true;
-        if (obj.GetType() != this.GetType()) return false;
-        return Equals((DiffHandler) obj);
-    }
-
-    public override int GetHashCode() {
-        unchecked {
-            return (base.GetHashCode() * 397) ^ Id;
-        }
-    }
 }
--- Assets/DiffImage.cs
+++ Assets/DiffImage.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.Serialization.Formatters.Binary;
+
+using Airion.Extensions;
 
 using Sirenix.OdinInspector;
+using Sirenix.Utilities;
 
 using UnityEditor;
 
@@ -115,8 +118,12 @@ public class DiffImage : MonoBehaviour {
     }
     
     Vector2 GetRectSpaceCoordinateFromPixel(Vector2 imageCoord, Image image, RectTransform imageRect) {
-        var locationRelativeToImage01 = new Vector2(imageCoord.x / image.sprite.texture.width, imageCoord.y / image.sprite.texture.height);
+        if (image.sprite == null) {
+            Err("Image not loaded");
+            return default;
+        }
         
+        var locationRelativeToImage01 = new Vector2(imageCoord.x / image.sprite.texture.width, imageCoord.y / image.sprite.texture.height);
         var imageAspectRatio = image.sprite.rect.height / image.sprite.rect.width;
         var rectAspectRatio = imageRect.rect.height / imageRect.rect.width;
         var imageRectInLocalScreenCoordinates = new Rect();
@@ -134,14 +141,13 @@ public class DiffImage : MonoBehaviour {
                 imageRect.rect.width, imageAspectRatio * imageRect.rect.width);
         }
         
-       
         var locationRelativeToImageInScreenCoordinates = new Vector2(imageRectInLocalScreenCoordinates.width * locationRelativeToImage01.x,
             imageRectInLocalScreenCoordinates.height * locationRelativeToImage01.y);
         var pivotCancelledLocation = new Vector2(imageRectInLocalScreenCoordinates.x + locationRelativeToImageInScreenCoordinates.x, 
             imageRectInLocalScreenCoordinates.y + locationRelativeToImageInScreenCoordinates.y);
         
         var pos = new Vector2(pivotCancelledLocation.x + imageRect.rect.x, pivotCancelledLocation.y + imageRect.rect.y);
-            
+
         return pos;
     }
 
@@ -156,23 +162,67 @@ public class DiffImage : MonoBehaviour {
     }
 
     [Button, ShowIf(nameof(IsPlaymode))]
-    void Load() {
+    void CreateNew() {
         _config.Image1.sprite = _image1;
         _config.Image2.sprite = _image2;
+    }
+    
+    [Button, ShowIf(nameof(IsPlaymode))]
+    void LoadJson() {
+        // Clear();
+        
+        var path = EditorUtility.OpenFilePanel("Load file", "", "json");
+        if (!File.Exists(path)) {
+            Err(path + " Not exist!");
+            return;
+        }
+
+        var jsonString = File.ReadAllText(path);
+        
+        var data = JsonUtility.FromJson<Data>(jsonString);
+        
+        LoadAndCreateImages(data);
+    }
+
+    void LoadAndCreateImages(Data data) {
+        // Load image through web request
+        foreach (var point in data.Points) {
+            CreateHandlerFromPoint(new Vector2(point.X, point.Y));
+        }
+    }
+
+    void CreateHandlerFromPoint(Vector2 point) {
+        var localPos1 =
+            GetRectSpaceCoordinateFromPixel(point, _config.Image1, _config.Image1.GetComponent<RectTransform>());
+
+        CreateHandler(localPos1, point, _config.Image1, _currentHandlerId);
+
+        var localPos2 =
+            GetRectSpaceCoordinateFromPixel(point, _config.Image2, _config.Image2.GetComponent<RectTransform>());
+
+
+        CreateHandler(localPos2, point, _config.Image2, _currentHandlerId);
+
+        _currentHandlerId++;
     }
 
     [Button, ShowIf(nameof(IsPlaymode))]
     void Clear() {
         _config.Image1.sprite = null;
         _config.Image2.sprite = null;
+        _config.Image1.transform.DestroyAllChildren();
+        _config.Image2.transform.DestroyAllChildren();
+        _handlers.Clear();
+        _currentHandlerId = 0;
     }
     
     [Button, ShowIf(nameof(IsPlaymode))]
-    void Save() {
+    void SaveJson() {
         var data = new Data();
         var points = new List<Point>();
-        var uniq = _handlers.Distinct();
-        foreach (var handler in uniq) {
+        var uniq = _handlers.DistinctBy(handler => handler.Id);
+        
+        foreach (var handler in uniq.ToArray()) {
             points.Add(new Point() {
                 X = handler.ImageSpaceCoordinates.x,
                 Y = handler.ImageSpaceCoordinates.y,
@@ -208,4 +258,15 @@ public class DiffImage : MonoBehaviour {
     void Err(string message) {
         Debug.LogError($"[{GetType()}] {message}");
     }
+    
+   
 }
+
+public static class Extension {
+    public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source,
+        Func<TSource, TKey> keySelector) {
+        var knownKeys = new HashSet<TKey>();
+        return source.Where(element => knownKeys.Add(keySelector(element)));
+    }
+}
+
