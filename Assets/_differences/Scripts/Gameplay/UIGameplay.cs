411010ce8af70ffb4ff4555effc43289d384b8a0
Eugene <me@airion.games>
2020-06-01 15:38:55 +0300

    Added orientation in gameplay mode

--- Assets/_differences/Scripts/Gameplay/UIGameplay.cs
+++ Assets/_differences/Scripts/Gameplay/UIGameplay.cs
@@ -7,11 +7,11 @@ using UnityEngine.UI;
 
 using Zenject;
 
-using static UnityEngine.Mathf;
-
 public class UIGameplay : MonoBehaviour {
-    [SerializeField] Image _image1 = default;
-    [SerializeField] Image _image2 = default;
+    [SerializeField] Image _image1Hor = default;
+    [SerializeField] Image _image2Hor = default;
+    [SerializeField] Image _image1Vert = default;
+    [SerializeField] Image _image2Vert = default;
     [SerializeField] TextMeshProUGUI _pointsCountText = default;
     [SerializeField] GameObject _diffVisualPrefab = default;
     
@@ -23,6 +23,7 @@ public class UIGameplay : MonoBehaviour {
     Data _data;
     int _pointsCount;
     int _currentPointsFound = 0;
+    (Image, Image) _currentImages;
 
     void Start() {
         var levelData = _database.GetLevelByNum(0);
@@ -35,8 +36,17 @@ public class UIGameplay : MonoBehaviour {
     }
     
     void Fill(Sprite image1, Sprite image2, Data levelData) {
-        _image1.sprite = image1;
-        _image2.sprite = image2;
+        _image1Hor.transform.parent.gameObject.SetActive(false);
+        _image1Vert.transform.parent.gameObject.SetActive(false);
+        
+        _currentImages = (_image1Hor, _image2Hor);
+        if (levelData.Orientation == Orientation.Vertical)
+            _currentImages = (_image1Vert, _image2Vert);
+        
+        _currentImages.Item1.transform.parent.gameObject.SetActive(true);
+
+        _currentImages.Item1.sprite = image1;
+        _currentImages.Item2.sprite = image2;
         _pointsCount = levelData.Points.Length;
         _currentPointsFound = 0;
         _points.Clear();
@@ -85,21 +95,21 @@ public class UIGameplay : MonoBehaviour {
     void CreateDiffsVisual(Point point) {
         var handler = Instantiate(_diffVisualPrefab);
         var handlerRect = handler.GetComponent<RectTransform>();
-        var image1Rect = _image1.GetComponent<RectTransform>();
-        var pos = DiffUtils.GetRectSpaceCoordinateFromPixel(point.Center, _image1, image1Rect);
-        handlerRect.SetParent(_image1.transform, false);
-        handlerRect.sizeDelta = new Vector2(DiffUtils.PixelWidthToRect(point.Width, image1Rect, _image1.sprite), 
-            DiffUtils.PixelHeightToRect(point.Height, image1Rect, _image1.sprite));
+        var image1Rect = _currentImages.Item1.GetComponent<RectTransform>();
+        var pos = DiffUtils.GetRectSpaceCoordinateFromPixel(point.Center, _currentImages.Item1, image1Rect);
+        handlerRect.SetParent(_currentImages.Item1.transform, false);
+        handlerRect.sizeDelta = new Vector2(DiffUtils.PixelWidthToRect(point.Width, image1Rect, _currentImages.Item1.sprite), 
+            DiffUtils.PixelHeightToRect(point.Height, image1Rect, _currentImages.Item1.sprite));
         handlerRect.localPosition = pos;
         
         var handler2 = Instantiate(_diffVisualPrefab);
         var handlerRect2 = handler2.GetComponent<RectTransform>();
-        var pos2 = DiffUtils.GetRectSpaceCoordinateFromPixel(point.Center, _image2,
-            _image2.GetComponent<RectTransform>());
-        handlerRect2.SetParent(_image2.transform, false);
-        var image2Rect = _image1.GetComponent<RectTransform>();
-        handlerRect2.sizeDelta = new Vector2(DiffUtils.PixelWidthToRect(point.Width, image2Rect, _image2.sprite), 
-            DiffUtils.PixelHeightToRect(point.Height, image2Rect, _image2.sprite));
+        var pos2 = DiffUtils.GetRectSpaceCoordinateFromPixel(point.Center, _currentImages.Item2,
+            _currentImages.Item2.GetComponent<RectTransform>());
+        handlerRect2.SetParent(_currentImages.Item2.transform, false);
+        var image2Rect = _currentImages.Item1.GetComponent<RectTransform>();
+        handlerRect2.sizeDelta = new Vector2(DiffUtils.PixelWidthToRect(point.Width, image2Rect, _currentImages.Item2.sprite), 
+            DiffUtils.PixelHeightToRect(point.Height, image2Rect, _currentImages.Item2.sprite));
         handlerRect2.localPosition = pos2;
     }
 }
