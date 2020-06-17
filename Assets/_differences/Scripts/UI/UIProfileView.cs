5279d13472a735745050d991884f6f1f5e53857d
Oleg Yanushevsky <asofi94@gmail.com>
2020-06-17 19:26:16 +0300

    Added profile view

---
+++ Assets/_differences/Scripts/UI/UIProfileView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using Doozy.Engine.UI;
+
+using UnityEngine;
+
+public class UIProfileView : MonoBehaviour {
+    UIView _currentView = default;
+
+    void Awake() {
+        _currentView = GetComponent<UIView>();
+    }
+
+    public void Show() {
+        _currentView.Show();
+    }
+
+    public void Hide() {
+        _currentView.Hide();
+    }
+}
