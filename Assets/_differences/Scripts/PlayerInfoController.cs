d354a6b5459153a3a20491c2ff1c8b8105581b39
Oleg Yanushevsky <asofi94@gmail.com>
2020-06-17 17:43:02 +0300

    Added profile info on global map.

---
+++ Assets/_differences/Scripts/PlayerInfoController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using UnityEngine;
+
+using Random = UnityEngine.Random;
+
+public class PlayerInfoController : MonoBehaviour {
+    [SerializeField] Sprite[] _profileIcons = default;
+    
+    int _playerIconId = default;
+    string _playerName = default;
+
+    const string NAME_ID = "name_id";
+    const string ICON_ID = "icon_id";
+
+    void Awake() {
+        LoadName();
+        LoadPlayerIcon();
+    }
+
+    void LoadName() {
+        _playerName = PlayerPrefs.GetString(NAME_ID, "user" + Random.Range(10000, 100000));
+    }
+
+    void SaveName() {
+        PlayerPrefs.SetString(NAME_ID, _playerName);
+    }
+
+    void LoadPlayerIcon() {
+        _playerIconId = PlayerPrefs.GetInt(ICON_ID, 0);
+    }
+
+    void SavePlayerIcon() {
+        PlayerPrefs.SetInt(ICON_ID, _playerIconId);
+    }
+
+    public string PlayerName {
+        get => _playerName;
+        set {
+            _playerName = value;
+            SaveName();
+        }
+    }
+
+    public Sprite SetIcon(int id) {
+        _playerIconId = id;
+        SavePlayerIcon();
+        return _profileIcons[Mathf.Clamp(id, 0, _profileIcons.Length)];
+    }
+    
+    public Sprite PlayerIcon => _profileIcons[Mathf.Clamp(_playerIconId, 0, _profileIcons.Length)];
+
+    public Sprite[] ProfileIcons => _profileIcons;
+}
---
+++ Assets/_differences/Scripts/UI/UIProfileInfoHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using TMPro;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+using Zenject;
+
+public class UIProfileInfoHandler : MonoBehaviour {
+    [SerializeField] Image _profileIcon = default;
+    [SerializeField] TMP_Text _nameLabel = default;
+
+    [Inject] PlayerInfoController _playerInfoController = default;
+
+    void Start() {
+        UpdateInfo();
+    }
+
+    void UpdateInfo() {
+        _profileIcon.sprite = _playerInfoController.PlayerIcon;
+        _nameLabel.text = _playerInfoController.PlayerName;
+    }
+}
