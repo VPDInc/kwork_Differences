dec45007911eaa2ac78f79d1e2b7c4c1f488e90b
Eugene <me@airion.games>
2020-06-18 15:36:30 +0300

    Completed UI daily reward

---
+++ Assets/Airion/DailyRewards/UI/UIDay.cs
@@ -0,0 +1,36 @@
+﻿using Airion.DailyRewards;
+
+using TMPro;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIDay : MonoBehaviour {
+    [SerializeField] TextMeshProUGUI _dayNum = default;
+    [SerializeField] TextMeshProUGUI _rewardAmount = default;
+    [SerializeField] Image _chest = default;
+    [SerializeField] Sprite _openedEmpty = default;
+    [SerializeField] Sprite _openedFull = default;
+    [SerializeField] Sprite _closed = default;
+
+    public void SetInfo(DayInfo info) {
+        if (info.Status == DayStatus.NotOpened && info.Additional.HasFlag(AdditionalDaySetting.Today)) {
+            _chest.sprite = _openedFull;
+        } else  if (info.Status == DayStatus.NotOpened) {
+            _chest.sprite = _closed;
+        } else {
+            _chest.sprite = _openedEmpty;
+        }
+        
+        _dayNum.text = (info.DayNum + 1).ToString() + " Day";
+        if (info.Additional.HasFlag(AdditionalDaySetting.Today)) {
+            _dayNum.text = "Today";
+        }
+        
+        if (info.Additional.HasFlag(AdditionalDaySetting.Tomorrow)) {
+            _dayNum.text = "Tomorrow";
+        }
+
+        _rewardAmount.text = ((CurrencyReward)info.Reward).Amount.ToString();
+    }
+}
---
+++ Assets/Airion/DailyRewards/UI/UIReceiveReward.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Airion.DailyRewards;
+
+using Doozy.Engine.UI;
+
+using TMPro;
+
+using UnityEngine;
+
+public class UIReceiveReward : MonoBehaviour {
+    public event Action Received;
+    
+    [SerializeField] TextMeshProUGUI _rewardedAmountText = default;
+    
+    [SerializeField] UIView _view = default;
+
+    public void Show(Reward reward) {
+        _rewardedAmountText.text = ((CurrencyReward)reward).Amount.ToString();
+        _view.Show();
+    }
+
+    public void Hide() {
+        _view.Hide();
+    }
+
+    public void OnReceiveClick() {
+        Received?.Invoke();
+    }
+
+}
