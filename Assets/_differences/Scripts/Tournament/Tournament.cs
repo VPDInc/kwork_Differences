c72536decccf85994e74e5a90de0f549b3971199
Eugene <me@airion.games>
2020-06-24 13:23:33 +0300

    Moved playfab login in project context

---
+++ Assets/_differences/Scripts/Tournament/Tournament.cs
@@ -0,0 +1,25 @@
+﻿using PlayFab;
+using PlayFab.ClientModels;
+
+using UnityEngine;
+
+public class Tournament : MonoBehaviour {
+    const string LEADERBOARD_NAME = "Tournament";
+    
+    void Start() {
+        PlayFabClientAPI.GetLeaderboard(new GetLeaderboardRequest() {StatisticName = LEADERBOARD_NAME}, 
+            OnLeaderboardLoadCompleted, OnLeaderboardLoadFailed);
+    }
+
+    void OnLeaderboardLoadCompleted(GetLeaderboardResult result) {
+        var entries = result.Leaderboard;
+    }
+    
+    void OnLeaderboardLoadFailed(PlayFabError error) {
+        Err(error.GenerateErrorReport());
+    }
+
+    void Err(string message) {
+        Debug.LogError($"[{GetType()}] {message}");
+    } 
+}
