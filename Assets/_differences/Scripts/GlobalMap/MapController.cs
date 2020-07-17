01bd50e5395f388c6090f3e790f184105f830f38
Oleg Yanushevsky <asofi94@gmail.com>
2020-07-17 21:30:52 +0300

    Episodes are spawning runtime now

--- Assets/_differences/Scripts/GlobalMap/MapController.cs
+++ Assets/_differences/Scripts/GlobalMap/MapController.cs
@@ -1,25 +1,45 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+
+using Airion.Extensions;
+
+using UnityEngine;
 
 using Zenject;
 
 public class MapController : MonoBehaviour {
     [SerializeField] GameObject _globalMapContainer = default;
+    [SerializeField] EpisodeInfo[] _episodeInfos = default;
+    [SerializeField] float _episodeLength = 0;
 
     [Inject] LevelController _levelController = default;
+    [Inject] DiContainer _diContainer = default;
 
-    EpisodeInfo[] _episodeInfos;
+    List<EpisodeInfo> _existedEpisodeInfos = new List<EpisodeInfo>();
+    float _currentOffset;
 
     void Start() {
         Init();
     }
 
     void Init() {
-        _episodeInfos = _globalMapContainer.GetComponentsInChildren<EpisodeInfo>();
+        _globalMapContainer.transform.DestroyAllChildren();
         int levelCount = 0;
         foreach (EpisodeInfo episodeInfo in _episodeInfos) {
-            episodeInfo.Init(levelCount);
-            levelCount += episodeInfo.LevelCount;
-            _levelController.AddLevelToList(episodeInfo.Levels);
+            var episode = _diContainer
+                          .InstantiatePrefab(episodeInfo, new Vector3(_currentOffset, 0, 0), Quaternion.identity,
+                                             _globalMapContainer.transform).GetComponent<EpisodeInfo>();
+
+            _currentOffset += _episodeLength;
+            _existedEpisodeInfos.Add(episode);
+            episode.Init(levelCount);
+            levelCount += episode.LevelCount;
+            _levelController.AddLevelToList(episode.Levels);
         }
+    }
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(new Vector3(-_episodeLength / 2, 0), new Vector3(_episodeLength / 2, 0));
     }
 }
