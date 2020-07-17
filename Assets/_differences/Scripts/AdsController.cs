5fccaff9296f969b8d1dda23723cc3352ba48a46
Oleg Yanushevsky <asofi94@gmail.com>
2020-07-17 15:26:57 +0300

    Added ads dummy

---
+++ Assets/_differences/Scripts/AdsController.cs
@@ -0,0 +1,28 @@
+﻿using Airion.Currency;
+
+using UnityEngine;
+
+using Zenject;
+
+public class AdsController : MonoBehaviour {
+    [SerializeField] int _adsCoinReward = 50;
+
+    [Inject] CurrencyManager _currencyManager = default;
+
+    Currency _currency = default;
+
+    const string CURRENCY_NAME = "Soft";
+    
+    void Start() {
+        _currencyManager.GetCurrency(CURRENCY_NAME);
+    }
+
+    public void RequestAd() {
+        //TODO: Add ads logic
+        RecieveReward();
+    }
+
+    void RecieveReward() {
+        _currency.Earn(_adsCoinReward);
+    }
+}
