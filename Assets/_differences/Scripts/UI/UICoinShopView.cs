99a4a7851302b4e95403687b1c064a216587e910
Oleg Yanushevsky <asofi94@gmail.com>
2020-07-27 18:15:28 +0300

    Imported iaps

--- Assets/_differences/Scripts/UI/UICoinShopView.cs
+++ Assets/_differences/Scripts/UI/UICoinShopView.cs
@@ -1,18 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
-public class UICoinShopView : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public class UICoinShopView : MonoBehaviour {
+    [SerializeField] UIOfferElement _offerElementPrefab = default;
+    
+    
 }
