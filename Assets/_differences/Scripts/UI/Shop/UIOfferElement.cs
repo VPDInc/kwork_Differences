0e07b11bcde301dc1ea31a82e1347cb4e3e76d58
Oleg Yanushevsky <asofi94@gmail.com>
2020-07-27 20:13:32 +0300

    Fixed warning

--- Assets/_differences/Scripts/UI/Shop/UIOfferElement.cs
+++ Assets/_differences/Scripts/UI/Shop/UIOfferElement.cs
@@ -5,7 +5,7 @@ using TMPro;
 using UnityEngine;
 
 public class UIOfferElement : MonoBehaviour {
-    [SerializeField] string _name;
+    [SerializeField] string _name = default;
     [SerializeField] int _buyAmount = default;
     
     [SerializeField] TMP_Text _titleText = default;
