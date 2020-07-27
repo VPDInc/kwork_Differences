1ed968effff6c94ce62ef8054590611cf4a1e594
Oleg Yanushevsky <asofi94@gmail.com>
2020-07-27 17:10:00 +0300

    Added shop view

---
+++ Assets/_differences/Scripts/UI/Shop/UIOfferElement.cs
@@ -0,0 +1,11 @@
+﻿using TMPro;
+
+using UnityEngine;
+
+public class UIOfferElement : MonoBehaviour {
+    [SerializeField] string _id = default;
+    [SerializeField] int _buyAmount = default;
+    [SerializeField] TMP_Text _titleText = default;
+    [SerializeField] TMP_Text _sellingAmountText = default;
+    [SerializeField] TMP_Text _costText = default;
+}
