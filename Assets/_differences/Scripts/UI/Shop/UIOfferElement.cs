f8a7ea85e584d26662bb48e9c4f84b0562e5365b
Oleg Yanushevsky <asofi94@gmail.com>
2020-07-27 20:09:13 +0300

    Added offers

--- Assets/_differences/Scripts/UI/Shop/UIOfferElement.cs
+++ Assets/_differences/Scripts/UI/Shop/UIOfferElement.cs
@@ -1,11 +1,31 @@
-﻿using TMPro;
+﻿using EasyMobile;
+
+using TMPro;
 
 using UnityEngine;
 
 public class UIOfferElement : MonoBehaviour {
-    [SerializeField] string _id = default;
+    [SerializeField] string _name;
     [SerializeField] int _buyAmount = default;
+    
     [SerializeField] TMP_Text _titleText = default;
+    [SerializeField] TMP_Text _descriptionText = default;
     [SerializeField] TMP_Text _sellingAmountText = default;
     [SerializeField] TMP_Text _costText = default;
+    
+    IAPProduct _iapProduct;
+
+    public string Name => _name;
+
+    public void Setup(string title, string description, string cost, IAPProduct product) {
+        _titleText.text = title;
+        _descriptionText.text = description;
+        _sellingAmountText.text = _buyAmount.ToString();
+        _costText.text = cost;
+        _iapProduct = product;
+    }
+
+    public void Buy() {
+        InAppPurchasing.Purchase(_iapProduct);
+    }
 }
--- Assets/_differences/Scripts/UI/UICoinShopView.cs
+++ Assets/_differences/Scripts/UI/UICoinShopView.cs
@@ -1,10 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Linq;
+
+using EasyMobile;
 
 using UnityEngine;
+using UnityEngine.Purchasing;
 
 public class UICoinShopView : MonoBehaviour {
-    [SerializeField] UIOfferElement _offerElementPrefab = default;
+    [SerializeField] Transform _offerContainer = default;
     
+    IAPProduct[] _products;
+
+    void Start() {
+        _products = EM_Settings.InAppPurchasing.Products;
+    }
+
+    void SetupOffers() {
+        var offers = _offerContainer.GetComponentsInChildren<UIOfferElement>();
+
+        foreach (UIOfferElement offer in offers) {
+            var iapProduct = EM_Settings.InAppPurchasing.Products.FirstOrDefault(x => x.Name == offer.Name);
+            ProductMetadata data = InAppPurchasing.GetProductLocalizedData(iapProduct.Name);
+            offer.Setup(data.localizedTitle, data.localizedDescription, data.localizedPriceString, iapProduct);
+        }
+    }
+
+    public void BuyCoinPack1() {
+        InAppPurchasing.Purchase(EM_IAPConstants.Product_Coin_Pack_1);
+    }
     
+    public void BuyCoinPack2() {
+        InAppPurchasing.Purchase(EM_IAPConstants.Product_Coin_Pack_2);
+    }
+    
+    public void BuyCoinPack3() {
+        InAppPurchasing.Purchase(EM_IAPConstants.Product_Coin_Pack_3);
+    }
+    
+    public void BuyCoinPack4() {
+        InAppPurchasing.Purchase(EM_IAPConstants.Product_Coin_Pack_4);
+    }
+    
+    public void BuyCoinPack5() {
+        InAppPurchasing.Purchase(EM_IAPConstants.Product_Coin_Pack_5);
+    }
 }
