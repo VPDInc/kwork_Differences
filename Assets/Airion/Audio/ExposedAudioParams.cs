11a7e24ed5c8632f3de0d4671a3eb8167bc5b2fc
Eugene <me@airion.games>
2020-07-28 13:18:46 +0300

    Integrated audio manager

---
+++ Assets/Airion/Audio/ExposedAudioParams.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class ExposedAudioParams : MonoBehaviour {
+    public event Action<string> Changed;
+    
+    [SerializeField] AudioMixer _mixer = default;
+    [SerializeField] string[] _volumeParams = default;
+
+    const string PARAM_PREFIX = "vol_";
+    const float LOW_VOLUME = -80;
+    
+    void Start() {
+        Load();
+    }
+
+    public void SwitchVolume(string param) {
+        if (_mixer.GetFloat(param, out var value)) {
+            if (Mathf.Approximately(value, LOW_VOLUME))
+                SetParamVolume(param, 0); 
+            else
+                SetParamVolume(param, LOW_VOLUME); 
+        }
+        
+        Save();
+    }
+
+    public bool IsMute(string param) {
+        if (_mixer.GetFloat(param, out var value)) 
+            return Mathf.Approximately(value, LOW_VOLUME);
+
+        return false;
+    }
+    
+    void Load() {
+        foreach (var volumeParam in _volumeParams) {
+            var val = PlayerPrefs.GetFloat(PARAM_PREFIX + volumeParam, 0);
+            SetParamVolume(volumeParam, val);
+        }
+    }
+
+    void Save() {
+        foreach (var param in _volumeParams) {
+            if (_mixer.GetFloat(param, out var value))
+                PlayerPrefs.SetFloat(PARAM_PREFIX + param, value);
+        }
+    }
+
+    void SetParamVolume(string paramName, float volume) {
+        _mixer.SetFloat(paramName, volume);
+        Changed?.Invoke(paramName);
+    }
+}
