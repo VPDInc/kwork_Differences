2c24476d742394165aa0d57d1c967b7ff2fd723d
Eugene <me@airion.games>
2020-08-04 16:10:50 +0300

    Fixed vibration exception

--- Assets/Airion/Vibration/Vibration.cs
+++ Assets/Airion/Vibration/Vibration.cs
@@ -10,44 +10,44 @@ using UnityEngine;
 using System.Collections;
 using System.Runtime.InteropServices;
 
-public class Vibration
-{
+public class Vibration {
 
 #if UNITY_IOS && !UNITY_EDITOR
 
     [DllImport ( "__Internal" )]
-    private static extern bool _HasVibrator ();
+    static extern bool _HasVibrator ();
 
     [DllImport ( "__Internal" )]
-    private static extern void _Vibrate ();
+    static extern void _Vibrate ();
 
     [DllImport ( "__Internal" )]
-    private static extern void _VibratePop ();
+    static extern void _VibratePop ();
 
     [DllImport ( "__Internal" )]
-    private static extern void _VibratePeek ();
+    static extern void _VibratePeek ();
 
     [DllImport ( "__Internal" )]
-    private static extern void _VibrateNope ();
+    static extern void _VibrateNope ();
 #endif
 
 #if UNITY_ANDROID && !UNITY_EDITOR
     public static AndroidJavaClass unityPlayer = new AndroidJavaClass ( "com.unity3d.player.UnityPlayer" );
     public static AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject> ( "currentActivity" );
-    public static AndroidJavaObject vibrator = currentActivity.Call<AndroidJavaObject> ( "getSystemService", "vibrator" );
+    public static AndroidJavaObject vibrator =
+ currentActivity.Call<AndroidJavaObject> ( "getSystemService", "vibrator" );
     public static AndroidJavaObject context = currentActivity.Call<AndroidJavaObject> ( "getApplicationContext" );
 #endif
 
     ///<summary>
     ///Only on iOS
     ///</summary>
-    public static void VibratePop ()
-    {
-        if ( !IsOnMobile () ) return;
+    public static void VibratePop() {
+        if (!IsOnMobile()) 
+            return;
 
 #if UNITY_IOS && !UNITY_EDITOR
         _VibratePop ();
- #elif UNITY_ANDROID && !UNITY_EDITOR
+#elif UNITY_ANDROID && !UNITY_EDITOR
         Vibrate (100);
 #endif
     }
@@ -55,23 +55,23 @@ public class Vibration
     ///<summary>
     ///Only on iOS
     ///</summary>
-    public static void VibratePeek ()
-    {
-        if ( !IsOnMobile () ) return;
+    public static void VibratePeek() {
+        if (!IsOnMobile()) 
+            return;
 
-        #if UNITY_IOS && !UNITY_EDITOR
+#if UNITY_IOS && !UNITY_EDITOR
         _VibratePeek ();
-        #elif UNITY_ANDROID && !UNITY_EDITOR
+#elif UNITY_ANDROID && !UNITY_EDITOR
         Vibrate (8);
-        #endif
+#endif
     }
 
     ///<summary>
     ///Only on iOS
     ///</summary>
-    public static void VibrateNope ()
-    {
-        if ( !IsOnMobile () ) return;
+    public static void VibrateNope() {
+        if (!IsOnMobile()) 
+            return;
 
 #if UNITY_IOS && !UNITY_EDITOR
         _VibrateNope ();
@@ -80,20 +80,19 @@ public class Vibration
 #endif
     }
 
-    public static void Vibrate ()
-    {
-        Handheld.Vibrate ();
+    public static void Vibrate() {
+        Handheld.Vibrate();
     }
 
     ///<summary>
     /// Only on Android
     /// https://developer.android.com/reference/android/os/Vibrator.html#vibrate(long)
     ///</summary>
-    public static void Vibrate ( long milliseconds )
-    {
-        if ( !IsOnMobile () ) return;
+    public static void Vibrate(long milliseconds) {
+        if (!IsOnMobile()) 
+            return;
 
-#if UNITY_ANDROID  && !UNITY_EDITOR
+#if UNITY_ANDROID && !UNITY_EDITOR
         vibrator.Call ( "vibrate", milliseconds );
 #elif UNITY_IOS && !UNITY_EDITOR
         _Vibrate ();
@@ -104,9 +103,9 @@ public class Vibration
     /// Only on Android
     /// https://proandroiddev.com/using-vibrate-in-android-b0e3ef5d5e07
     ///</summary>
-    public static void Vibrate ( long[] pattern, int repeat )
-    {
-        if ( !IsOnMobile () ) return;
+    public static void Vibrate(long[] pattern, int repeat) {
+        if (!IsOnMobile()) 
+            return;
 
 #if UNITY_ANDROID && !UNITY_EDITOR
         vibrator.Call ( "vibrate", pattern, repeat );
@@ -115,14 +114,17 @@ public class Vibration
 #endif
     }
 
-    public static bool HasVibrator ()
-    {
-        if ( !IsOnMobile () ) return false;
+    public static bool HasVibrator() {
+        if (!IsOnMobile()) 
+            return false;
+
+#pragma warning disable 0162 // Unreachable code detected
 
 #if UNITY_ANDROID && !UNITY_EDITOR
         AndroidJavaClass contextClass = new AndroidJavaClass ( "android.content.Context" );
         string Context_VIBRATOR_SERVICE = contextClass.GetStatic<string> ( "VIBRATOR_SERVICE" );
-        AndroidJavaObject systemService = context.Call<AndroidJavaObject> ( "getSystemService", Context_VIBRATOR_SERVICE );
+        AndroidJavaObject systemService =
+ context.Call<AndroidJavaObject> ( "getSystemService", Context_VIBRATOR_SERVICE );
         if ( systemService.Call<bool> ( "hasVibrator" ) ) {
             return true;
         } else
@@ -130,29 +132,30 @@ public class Vibration
 #elif UNITY_IOS && !UNITY_EDITOR
         return _HasVibrator ();
 #endif
+#pragma warning restore 0162 // Unreachable code detected
         return false;
     }
 
     ///<summary>
     ///Only on Android
     ///</summary>
-    public static void Cancel ()
-    {
-        if ( !IsOnMobile () ) return;
+    public static void Cancel() {
+        if (!IsOnMobile()) 
+            return;
 #if UNITY_ANDROID && !UNITY_EDITOR
         vibrator.Call ( "cancel" );
 #endif
     }
-    private static bool IsOnMobile ()
-    {
-        #if UNITY_EDITOR
-            return false;
-        #endif
-        #pragma warning disable 0162 // Unreachable code detected
-        if ( Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer )
+
+    static bool IsOnMobile() {
+#if UNITY_EDITOR
+        return false;
+#endif
+#pragma warning disable 0162 // Unreachable code detected
+        if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer)
             return true;
 
         return false;
-        #pragma warning restore 0162 // Unreachable code detected
+#pragma warning restore 0162 // Unreachable code detected
     }
 }
