df6382264e63fe30e4172c468f01d6434a2ebaa9
Oleg Yanushevsky <asofi94@gmail.com>
2020-08-06 17:28:17 +0300

    Start working on tips shine fx

---
+++ Assets/_differences/Scripts/Gameplay/UI/UIShineBoosterFX.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using DG.Tweening;
+
+using UnityEngine;
+
+public class UIShineBoosterFX : MonoBehaviour {
+    [SerializeField] RectTransform _fxTransform = default;
+    [SerializeField] float _topPoint = 1;
+    [SerializeField] float _botPoint = -1;
+    [SerializeField] float _duration = 0.5f;
+
+    void OnEnable() {
+        Reset();
+    }
+
+    public void Play() {
+        Reset();
+        var targetPos = _fxTransform.anchoredPosition;
+        targetPos.y = _botPoint;
+        _fxTransform.DOAnchorPos(targetPos, _duration).SetId(this);
+    }
+
+    void Reset() {
+        DOTween.Kill(this);
+        var currentPos = _fxTransform.anchoredPosition;
+        currentPos.y = _topPoint;
+        _fxTransform.anchoredPosition = currentPos;
+    }
+}
