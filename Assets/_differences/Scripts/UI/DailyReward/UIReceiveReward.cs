9e8dbf51b3c2cb715b56948e421af0f7df35804a
Oleg Yanushevsky <asofi94@gmail.com>
2020-08-18 15:34:32 +0300

    Added daily fx

--- Assets/_differences/Scripts/UI/DailyReward/UIReceiveReward.cs
+++ Assets/_differences/Scripts/UI/DailyReward/UIReceiveReward.cs
@@ -14,8 +14,17 @@ public class UIReceiveReward : MonoBehaviour {
     [SerializeField] TextMeshProUGUI _rewardedAmountText = default;
     
     [SerializeField] UIView _view = default;
+    
+    [SerializeField] UITrailEffect _uiTrailEffectPrefab = default;
+    [SerializeField] RectTransform _fxStartTransform = default;
+    [SerializeField] RectTransform _fxTargetTransform = default;
+    [SerializeField] float _amountDivider = 5;
+    [SerializeField] float _pauseBetweenSpawns = 0.02f;
+
+    int _rewardAmount;
 
     public void Show(Reward reward) {
+        _rewardAmount = ((CurrencyReward) reward).Amount;
         _rewardedAmountText.text = ((CurrencyReward)reward).Amount.ToString();
         _view.Show();
     }
@@ -25,7 +34,14 @@ public class UIReceiveReward : MonoBehaviour {
     }
 
     public void OnReceiveClick() {
+        SetupTrailEffect(_rewardAmount);
         Received?.Invoke();
     }
 
+    void SetupTrailEffect(int coinsAmount) {
+        for (int i = 0; i < coinsAmount / _amountDivider; i++) {
+            var coinFx = Instantiate(_uiTrailEffectPrefab, _fxStartTransform);
+            coinFx.Setup(_fxTargetTransform.position, _pauseBetweenSpawns * i);
+        }
+    }
 }
