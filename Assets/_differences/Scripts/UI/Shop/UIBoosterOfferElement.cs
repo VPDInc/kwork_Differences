032bd6339893c6c6cf4995f2837946bde6b651ff
Oleg Yanushevsky <asofi94@gmail.com>
2020-08-18 14:59:31 +0300

    Added booster shop fx

--- Assets/_differences/Scripts/UI/Shop/UIBoosterOfferElement.cs
+++ Assets/_differences/Scripts/UI/Shop/UIBoosterOfferElement.cs
@@ -17,10 +17,15 @@ public class UIBoosterOfferElement : MonoBehaviour {
     [SerializeField] Image _icon = default;
     [SerializeField] TMP_Text _amountLabel = default;
     [SerializeField] TMP_Text _costLabel = default;
+    
+    [SerializeField] UITrailEffect _uiTrailEffectPrefab = default;
+    [SerializeField] float _amountDivider = 5;
+    [SerializeField] float _pauseBetweenSpawns = 0.02f;
 
     [Inject] CurrencyManager _currencyManager = default;
     [Inject] LevelController _levelController = default;
 
+    Transform _fxStartTransform;
     Currency _coinsCurrency;
     Currency _currency;
     int _amountToBuy;
@@ -33,7 +38,7 @@ public class UIBoosterOfferElement : MonoBehaviour {
         _coinsCurrency = _currencyManager.GetCurrency(COINS_CURRENCY_ID);
     }
 
-    public void Setup(Currency currency, string title, Sprite icon, int amount, int cost) {
+    public void Setup(Currency currency, string title, Sprite icon, int amount, int cost, Transform fxStartTransform) {
         _currency = currency;
         _amountToBuy = amount;
         _cost = cost;
@@ -42,6 +47,8 @@ public class UIBoosterOfferElement : MonoBehaviour {
         _icon.sprite = icon;
         _amountLabel.text = amount.ToString();
         _costLabel.text = cost + " <sprite=0>";
+
+        _fxStartTransform = fxStartTransform;
     }
 
     public void Buy() {
@@ -49,8 +56,16 @@ public class UIBoosterOfferElement : MonoBehaviour {
             _coinsCurrency.Spend(_cost);
             Analytic.CurrencySpend(_cost, "booster-bought", _currency.name, _levelController.LastLevelNum);
             _currency.Earn(_amountToBuy);
+            SetupTrailEffect(100);
         } else {
             GameEventMessage.SendEvent(OPEN_STORE_EVENT_ID);
+        }
+    }
+    
+    void SetupTrailEffect(int coinsAmount) {
+        for (int i = 0; i < coinsAmount / _amountDivider; i++) {
+            var coinFx = Instantiate(_uiTrailEffectPrefab, _fxStartTransform);
+            coinFx.Setup(transform.position, _pauseBetweenSpawns * i);
         }
     }
 }
