d0dbeab0642edeb1de595359d21023c910a1f1e6
Valentine <forwow30@gmail.com>
2021-01-11 11:56:35 +0200

    Add start architecture from new currency's, add callback action with end play fx animation.

--- Assets/_differences/Scripts/Gameplay/UI/UITrailEffect.cs
+++ Assets/_differences/Scripts/Gameplay/UI/UITrailEffect.cs
@@ -1,17 +1,18 @@
 ﻿using DG.Tweening;
-
+using System;
 using UnityEngine;
-using UnityEngine.UI.Extensions;
 
-public class UITrailEffect : MonoBehaviour {
+public class UITrailEffect : MonoBehaviour 
+{
     [SerializeField] float _moveDuration = 0.5f;
     [SerializeField] float _scaleDuration = 0.2f;
 
-    public void Setup(Vector2 target, float delay = 0) {
+    public void Setup(Vector2 target, float delay = 0, Action OnSuccess = null) 
+    {
         var seq = DOTween.Sequence();
         seq.AppendInterval(delay);
         seq.Append(transform.DOMove(target, _moveDuration));
         seq.Append(transform.DOScale(0, _scaleDuration));
-        seq.AppendCallback(() => Destroy(gameObject));
+        seq.AppendCallback( delegate { Destroy(gameObject); OnSuccess?.Invoke(); });
     }
 }
---
+++ Assets/_differences/Scripts/NewGeneration/Currency/CurrencyService.cs
@@ -0,0 +1,27 @@
+﻿
+
+using Airion.Currency;
+using Zenject;
+
+namespace _differences.Scripts.NewGeneration.Currency
+{
+    public class CurrencyService : ICurrencyService
+    {
+        [Inject] CurrencyManager _currencyManager = default;
+
+        public void Earn(int count, string type)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Airion.Currency.Currency GetCurrency(string type)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Spend(int count, string type)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
