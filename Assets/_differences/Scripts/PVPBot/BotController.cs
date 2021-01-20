7887884f14e091fad5247a9e87d2babbfed09a25
Valentine <forwow30@gmail.com>
2021-01-20 20:48:54 +0200

    Add base arhitecture for pvp bot

---
+++ Assets/_differences/Scripts/PVPBot/BotController.cs
@@ -0,0 +1,11 @@
+﻿namespace _differences.Scripts.PVPBot
+{
+    public class BotController
+    {
+        public Bot GetBot()
+        {
+            BotBuilder botBuilder = new BotBuilder();
+            return botBuilder.GetBot();
+        }
+    }
+}
---
+++ Assets/_differences/Scripts/PVPBot/Other/Logger.cs
@@ -0,0 +1,10 @@
+﻿namespace _differences.Scripts.PVPBot.Other
+{
+    public class Logger
+    {
+        public static void Log(string textLog)
+        {
+            UnityEngine.Debug.Log(textLog);
+        }
+    }
+}
