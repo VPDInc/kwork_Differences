dd7dfb0db6bcfa02799342fbc4900f272f59ff3a
Valentine <forwow30@gmail.com>
2021-01-28 16:38:31 +0200

    Upd game config

--- Assets/_differences/Scripts/Configs/GameConfig.cs
+++ Assets/_differences/Scripts/Configs/GameConfig.cs
@@ -7,13 +7,30 @@ namespace _differences.Scripts.Configs
     [Serializable]
     public struct GameConfig
     {
-        public DifficultyBot[] Difficulties;
+        public BotConfig BotConfig;
+    }
+
+    [Serializable]
+    public struct BotConfig
+    {
+        [Header("InitialDifference values")]
+        public int MinTimeFindInitialDifference;
+        public int MaxTimeFindInitialDifference;
+        [Header("NextDifference values")]
+        public int MinTimeFindNextDifference;
+        public int MaxTimeFindNextDifference;
+
+        //public int 
+        //[Header("")]
+        public DifficultyBot[] BotDifficulties;
     }
 
     [Serializable]
     public struct DifficultyBot
     {
-        public BotDifficulty DifficultyValue;
+        public string DifficultyValue;
         public float PercentValue;
     }
+
+
 }
