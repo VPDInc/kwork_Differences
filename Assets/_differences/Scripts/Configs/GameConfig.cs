8bdccd75433779d91d08ebce09472deadd50aa39
Valentine <forwow30@gmail.com>
2021-01-30 12:44:12 +0200

    Add new template art for new bot System

--- Assets/_differences/Scripts/Configs/GameConfig.cs
+++ Assets/_differences/Scripts/Configs/GameConfig.cs
@@ -16,9 +16,23 @@ namespace _differences.Scripts.Configs
         [Header("InitialDifference values")]
         public int MinTimeFindInitialDifference;
         public int MaxTimeFindInitialDifference;
+
         [Header("NextDifference values")]
         public int MinTimeFindNextDifference;
         public int MaxTimeFindNextDifference;
+
+        [Header("TimeFindLastDifference values")]
+        public int MinTimeFindLastDifference; //70
+        public int MaxTimeFindLastDifference; //130
+
+        //закончилось внремья на раунд при последней разницы от 5 до 10 2 переменных
+
+
+        //Сравнение сколько разниц найдено у бота и игрока, 5 елементов
+        public CompareDifferences[] CompareDifferences;
+        
+
+        //ускорений режим от 2 до 10 секунд
 
         //public int 
         //[Header("")]
@@ -32,5 +46,12 @@ namespace _differences.Scripts.Configs
         public float PercentValue;
     }
 
+
+    [Serializable]
+    public struct CompareDifferences
+    {
+        public int CountDiffernces;
+        public int PercentStartFastFindDifferences;
+    }
 
 }
